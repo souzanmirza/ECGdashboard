d4b0087925e82dd708d239262bef724d1bc3965e
Souzan <souzanmirza@gmail.com>
2018-09-19 23:14:42 -0400

    Renaming file.

--- src/spark/spark_consumer.py
+++ src/spark/spark_consumer.py
@@ -3,7 +3,7 @@ import sys
 #sys.path.append('../python/')
 #import helpers
 
-spark_config = helpers.parse_config('../../.config/spark.config')
+#spark_config = helpers.parse_config('../../.config/spark.config')
 
 #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 consumer.py 52.201.50.203:9092 ecg-data'
 
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     ssc = StreamingContext(sc, 2)
     brokers = 'ec2-52-201-50-203.compute-1.amazonaws.com:9092'
     topic = 'ecg-topic'
-    kvs = KafkaUtils.createDirectStream(ssc, [topic],{'metadata.broker.list': brokers})
-    lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+    kafkastream = KafkaUtils.createDirectStream(ssc, [topic],{'metadata.broker.list': brokers})
+    lines = kafkastream.map(lambda x: x[i] for i in range(len(x)))
+    counts = lines.map(lambda line: line.split(' '))
     #need to fix this map/reduce statement cuz I have no idea what it's doing.
     #need to remove logs or save them to s3 bucket
     counts.pprint()
