4c4994a09ef7937e218552dfa315e8f51f0fab76
souzan <souzanmirza@gmail.com>
2018-09-20 17:27:48 +0000

    updaed name of consumer in bash file

--- src/spark/runspark.sh
+++ src/spark/runspark.sh
@@ -1,2 +1,2 @@
-/usr/local/spark/bin/spark-submit --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/consumer.py
+/usr/local/spark/bin/spark-submit --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer.py
 #Pull repo to node first
