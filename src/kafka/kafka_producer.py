6b6bc7829a1bfa0f620132c1eabd73467fd8be59
Souzan <souzanmirza@gmail.com>
2018-09-20 12:11:33 -0400

    Added in message buffer parser. It is very slow.

--- src/kafka/kafka_producer.py
+++ src/kafka/kafka_producer.py
@@ -7,16 +7,42 @@ import time
 from datetime import datetime
 sys.path.append('../python/')
 import helpers
+import binascii
+import numpy as np
+
+fs = 360
 
 class Producer(object):
 
     def __init__(self, addr):
         self.kafka_config = helpers.parse_config('../../.config/kafka.config')
-        self.producer = KafkaProducer(bootstrap_servers=addr)
+        self.producer = KafkaProducer(value_serializer = lambda v:  v.encode('utf-8'),
+                                      bootstrap_servers=addr)
+
+    def produce_msgs_buffer(self, file_key):
+        msg_cnt = 0
+
+        while True:
+
+            s3 = boto3.client('s3')
+            obj = s3.get_object(Bucket=self.kafka_config['bucket'],
+                                Key="%s_signals.txt"%file_key)['Body']
+
+            messagebuffer = np.chararray((fs+2), itemsize=10)
+            messagebuffer[0] = file_key
+            messagebuffer[1] = datetime.now().strftime("%Y%m%d-%H%M%S")
+            for i in range(2, fs+2):
+                # this takes some time to read in...
+                messagebuffer[i] = '%0.3f'%(float(obj._raw_stream.readline().decode().split(' ')[0]))
+            self.producer.send(self.kafka_config['topic'], value = messagebuffer)
+            msg_cnt += 1
+            print(messagebuffer[np.newaxis:,], len(messagebuffer))
+            time.sleep(0.001)
 
     def produce_msgs(self, file_key):
         """
         produces messages and sends them to topic
+        to do: tag all samples from same second with same timestamp (fs = 360 ie. 360 samples / second)
         """
         msg_cnt = 0
 
@@ -24,10 +50,10 @@ class Producer(object):
 
             s3 = boto3.client('s3')
             obj = s3.get_object(Bucket=self.kafka_config['bucket'],
-                                Key="%s_signals.txt"%file_key)
+                                Key="%s_signals.txt" % file_key)
             for line in obj['Body'].iter_lines():
                 linesplit = line.decode().split(' ')
-                time_field = datetime.now().strftime("%Y%m%d %H%M%S")
+                time_field = datetime.now().strftime("%Y%m%d-%H%M%S")
                 str_fmt = "{},{},{}mv"
                 message_info = str_fmt.format(file_key,
                                               time_field,
@@ -49,4 +75,4 @@ if __name__ == "__main__":
     ip_addr = str(args[1])
     file_key = str(args[2])
     prod = Producer(ip_addr)
-    prod.produce_msgs(file_key)
+    prod.produce_msgs_buffer(file_key)
