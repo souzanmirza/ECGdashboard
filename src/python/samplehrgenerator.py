ec342c6d107cb713808bc0adfed6db5bb9f234dc
Souzan <souzanmirza@gmail.com>
2018-09-22 15:22:51 -0400

    Fixed timestamps for fake ecg generator.

--- src/python/samplehrgenerator.py
+++ src/python/samplehrgenerator.py
@@ -1,12 +1,11 @@
 import numpy as np
 import datetime
 
-def constantpeaks(bpm, fs, ecg =np.array([]), seconds=6):
-    fulldate = datetime.datetime.now()
+def constantpeaks(bpm, fs, ecg =np.array([]), seconds = 6, fulldate=datetime.datetime.now()):
     length = fs*seconds
     fake_ecg = np.zeros((3, length))
-    RR_interval = 1/bpm
-    peaks = range(0,length,round(RR_interval * fs))
+    beatspersecond = 1/bpm * 60
+    peaks = range(0,length,round(beatspersecond * fs))
     timestamps = []
     fake_ecg[:,peaks] = 1
     for i in range(length):
@@ -20,9 +19,10 @@ def constantpeaks(bpm, fs, ecg =np.array([]), seconds=6):
     fake_ecg = np.hstack([timestamps, fake_ecg.transpose()])
     if len(ecg) > 0:
         fake_ecg = np.vstack([ecg, fake_ecg])
-    return fake_ecg
+    return fake_ecg, fulldate
 
 if __name__ == '__main__':
-    fakeecg1 = constantpeaks(60, 360, seconds = 10)
-    fakeecg2 = constantpeaks(72, 360, seconds = 10, ecg = fakeecg1)
+    fulldate = datetime.datetime.now()
+    fakeecg1, fulldate = constantpeaks(60, 360, seconds = 10, fulldate=fulldate)
+    fakeecg2, fulldate = constantpeaks(72, 360, seconds = 10, ecg = fakeecg1, fulldate=fulldate)
     np.savetxt('../../data/fake003_signals.txt', fakeecg2, delimiter = ',', fmt='%s')
