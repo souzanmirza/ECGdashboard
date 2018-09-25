315ddc06744bea746b6aa5403d28748c0c75545c
Souzan <souzanmirza@gmail.com>
2018-09-25 15:10:37 -0400

    using foreachpartition to process and write instantaneous hr to db

--- src/spark/spark_consumer_class.py
+++ src/spark/spark_consumer_class.py
@@ -23,6 +23,7 @@ def accum(a):
     a.add(1)
     return a.value
 
+
 class SparkConsumer:
 
     def __init__(self, spark_config_infile, postgres_config_infile):
@@ -40,26 +41,18 @@ class SparkConsumer:
         self.logger.info('Opened spark Context')
         self.kafkastream = self.openKafka()
         self.a = self.sc.accumulator(0)
-        self.connpool = self.openDBConnectionPool()
 
     def start(self):
         self.ssc.start()
         self.logger.info('Spark context started')
         self.ssc.awaitTermination()
         self.logger.info('Spark context terminated')
-        
+
     def openKafka(self):
         kafkastream = KafkaUtils.createDirectStream(self.ssc, [self.spark_config['topic']],
-                                                         {'metadata.broker.list': self.spark_config['ip-addr']})
+                                                    {'metadata.broker.list': self.spark_config['ip-addr']})
         self.logger.info('Connected kafka stream to spark context')
         return kafkastream
-
-    def openDBConnectionPool(self):
-        connpool = pool.SimpleConnectionPool(1, 10, host=self.postgres_config['host'],
-                                             database=self.postgres_config['database'], port=self.postgres_config['port'],
-                                             user=self.postgres_config['user'], password=self.postgres_config['password'])
-        self.logger.info('Created db pooled connections')
-        return connpool
 
     def run(self):
         lines = self.kafkastream.map(lambda x: x[1])
@@ -67,19 +60,19 @@ class SparkConsumer:
 
         raw_record = lines.map(lambda line: line.encode('utf-8')). \
             map(lambda line: line.split(','))
-        raw_record.foreachRDD(lambda x: (self.insert_partition(accum(self.a), x)))
+        raw_record.foreachRDD(lambda x: (self.insert_sample(accum(self.a), x)))
         self.logger.info('Saved records to db')
         self.ssc.start()
         self.logger.info('Spark context started')
         self.ssc.awaitTermination()
         self.logger.info('Spark context terminated')
-        '''
+
         record_interval = raw_record.map(lambda line: (line[0], line[1:])). \
             groupByKey().map(lambda x: (x[0], np.array(list(x[1]))))
 
         record_interval.foreachRDD(lambda x: self.calculateHR(self.a.value, x))
         self.logger.info('Calculated HR for 2s spark stream mini-batch')
-        '''
+
 
     def findHR(self, ts, ecg):
         self.logger.info('fxn findHR')
@@ -105,84 +98,64 @@ class SparkConsumer:
             self.logger.debug('Invalid HR returned')
             return -1
 
-
-    def _calculateHR(self, a, x):
-        print('fxn _calculateHR')
-        self.logger.info('fxn _calculateHR')
-        sqlcmd = "INSERT INTO inst_hr(batchnum, signame, hr1, hr2, hr3) " \
-                 "VALUES (%s, %s, %s, %s, %s)"
-        # for key in rdd_.keys().collect():
-        #     x = rdd_.lookup(key)
-        ts_str = x[1][:, 0]
-        # print(ts_str)
-        if len(ts_str) > 3:
-            # print('passed: ', ts_str.shape)
-            ts_datetime = [datetime.strptime(ts_str[i], '%Y-%m-%d %H:%M:%S.%f') for i in range(len(ts_str))]
-            ts_datetime = np.array(ts_datetime)
-            ecg1 = np.array(x[1][:, 1]).astype(float)
-            # print(ecg1)
-            ecg2 = np.array(x[1][:, 2]).astype(float)
-            ecg3 = np.array(x[1][:, 3]).astype(float)
-            sampleHR = [x[0], self.findHR(ts_datetime, ecg1), self.findHR(ts_datetime, ecg2), self.findHR(ts_datetime, ecg3)]
-            print(sampleHR)
-            self._insert(self.connpool, sqlcmd, a, sampleHR)
-        else:
-            self.logger.debug('No HR returned')
-
-
     def calculateHR(self, a, record):
         self.logger.info('fxn calculateHR')
-        record.foreach(lambda x: self._calculateHR(a, x))
-
-
-    def get_connection(self):
-        self.logger.info('fxn get_connection')
-        conn = self.connpool.getconn()
-        try:
-            yield conn
-        finally:
-            self.connpool.putconn(conn)
-
-
-    def _insert(self, sqlcmd, a, x):
-        # print(x)
-        # print('fxn _insert ')
-        self.logger.info('fxn _insert')
-        for conn in self.get_connection().__iter__():
-            # print(conn)
-            try:
-                cur = conn.cursor()
-                cur.execute(sqlcmd, [a] + x)
-                cur.close()
-                conn.commit()
-            except Exception as e:
-                self.logger.debug('Exception %s, rolling back' % e)
-                print('Exception %s, rolling back' % e)
-                conn.rollback()
-
-
-    def insert(self, a, record):
-        # print('fxn insert ', logger)
-        print('accum in insert %s' % a)
-        self.logger.info('fxn insert')
-        sqlcmd = "INSERT INTO signal_samples(batchnum, signame, time, ecg1, ecg2, ecg3) " \
-                 "VALUES (%s, %s, %s, %s, %s, %s)"
-        record.foreach(lambda x: self._insert(sqlcmd, a, x))
-
-
-    def insert_partition(self, a, record):
-        # print('fxn insert ', logger)
-        #print('accum in insert', a)
-        self.logger.info('fxn insert')
-        sqlcmd = "INSERT INTO signal_samples(batchnum, signame, time, ecg1, ecg2, ecg3) " \
-                 "VALUES ("+str(a)+", %s, %s, %s, %s, %s) " \
-                 "ON CONFLICT DO NOTHING"
-        #print(sqlcmd)
-        #print('fxn insert_partition: record is %s '%type(record))
-        #record_bcast = self.sc.broadcast(record)
+        sqlcmd = "INSERT INTO inst_hr(batchnum, signame, hr1, hr2, hr3) " \
+                 "VALUES (" + str(a) + ", %s, %s, %s, %s) " \
+                                       "ON CONFLICT DO NOTHING"
         postgres_config = self.postgres_config
-        def _insert_partition(x):
-            #print('x in _insert_partition', list(x), type(x))
+
+        def _calculateHR(x):
+            #print('fxn _calculateHR')
+            #self.logger.info('fxn _calculateHR')
+            ts_str = x[1][:, 0]
+            if len(ts_str) > 3:
+                ts_datetime = [datetime.strptime(ts_str[i], '%Y-%m-%d %H:%M:%S.%f') for i in range(len(ts_str))]
+                ts_datetime = np.array(ts_datetime)
+                ecg1 = np.array(x[1][:, 1]).astype(float)
+                ecg2 = np.array(x[1][:, 2]).astype(float)
+                ecg3 = np.array(x[1][:, 3]).astype(float)
+                instHR = [x[0], self.findHR(ts_datetime, ecg1), self.findHR(ts_datetime, ecg2),
+                            self.findHR(ts_datetime, ecg3)]
+                print(instHR)
+
+                def _insert_inst_hr(x):
+                    print('in fx _insert_inst_hr %s'%sqlcmd)
+                    try:
+                        conn = psycopg2.connect(host=postgres_config['host'],
+                                                database=postgres_config['database'],
+                                                port=postgres_config['port'],
+                                                user=postgres_config['user'],
+                                                password=postgres_config['password'])
+                        cur = conn.cursor()
+                        cur.executemany(sqlcmd, list(x))
+                        conn.commit()
+                        cur.close()
+                        conn.close()
+                    except Exception as e:
+                        # self.logger.debug('Exception %s' % e)
+                        print('Exception %s' % e)
+
+                _insert_inst_hr(instHR)
+            else:
+                self.logger.debug('No HR returned')
+
+        record.foreachPartition(_calculateHR)
+
+    def insert_sample(self, a, record):
+        # print('fxn insert ', logger)
+        # print('accum in insert', a)
+        self.logger.info('fxn insert')
+        sqlcmd = "INSERT INTO signal_samples(batchnum, signame, time, ecg1, ecg2, ecg3) " \
+                 "VALUES (" + str(a) + ", %s, %s, %s, %s, %s) " \
+                                       "ON CONFLICT DO NOTHING"
+        #print(sqlcmd)
+        # print('fxn insert_partition: record is %s '%type(record))
+        # record_bcast = self.sc.broadcast(record)
+        postgres_config = self.postgres_config
+
+        def _insert_sample(x):
+            # print('x in _insert_partition', list(x), type(x))
             # print(x)
             # print('fxn _insert ')
             # self.logger.info('fxn _insert')
@@ -192,26 +165,21 @@ class SparkConsumer:
                                         port=postgres_config['port'],
                                         user=postgres_config['user'],
                                         password=postgres_config['password'])
+                cur = conn.cursor()
+                cur.executemany(sqlcmd, list(x))
+                # cur.execute("DEALLOCATE inserts")
+                # cur.execute(sqlcmd, [a] + list(x))
+                conn.commit()
+                cur.close()
+                conn.close()
             except Exception as e:
                 # self.logger.debug('Exception %s' % e)
                 print('Exception %s' % e)
-
-            cur = conn.cursor()
-            cur.executemany(sqlcmd, list(x))
-            #cur.execute("DEALLOCATE inserts")
-            #cur.execute(sqlcmd, [a] + list(x))
-            conn.commit()
-            cur.close()
-            conn.close()
-        record.foreachPartition(_insert_partition) #(postgres_config, sqlcmd, a, list(x)))
-
+        record.foreachPartition(_insert_sample)
 
 if __name__ == '__main__':
     spark_config_infile = '../../.config/spark.config'
     postgres_config_infile = '../../.config/postgres.config'
     consumer = SparkConsumer(spark_config_infile, postgres_config_infile)
-    #consumer.start()
+    # consumer.start()
     consumer.run()
-
-
-
