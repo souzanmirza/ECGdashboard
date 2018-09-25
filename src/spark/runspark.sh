86d6080487552b627df1315a20fc84ee571d00f1
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2018-09-25 19:43:07 +0000

    changed spark consumer script call

--- src/spark/runspark.sh
+++ src/spark/runspark.sh
@@ -1,2 +1,2 @@
-/usr/local/spark/bin/spark-submit --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer.py
+/usr/local/spark/bin/spark-submit --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py
 #Pull repo to node first
