5f6b2899cf66d98f2755c10e07c54943d37767a9
Souzan <souzanmirza@gmail.com>
2018-09-27 17:01:10 -0400

    python script for launching spark on whole cluster

---
+++ src/spark/spawn_spark_consumer.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../python')
+import helpers
+import os
+
+spark_config = helpers.parse_config('../../.config/spark.config')
+ipaddr = spark_config['master-ip-addr'].split(',')
+
+os.system("peg sshcmd-cluster spark-cluster '/usr/local/spark/bin/spark-submit --master spark://%s:7077 "
+          "--deploy-mode cluster --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py'"%(ipaddr))
