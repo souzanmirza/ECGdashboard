b713a0fe46341b838b84583ab6f584629ea1ca19
Souzan <souzanmirza@gmail.com>
2018-09-27 10:48:30 -0400

    removing inputs to spawn kafka streams call to kafka_producer

--- src/kafka/spawn_kafka_streams.sh
+++ src/kafka/spawn_kafka_streams.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 BUCKET=$1
 RECORDLIST=$2
-IP_ADDR=$3
-SESSION=$4
+SESSION=$3
 
 aws s3 cp s3://$BUCKET/$RECORDLIST $RECORDLIST
 readarray recordsarray < $RECORDLIST
@@ -13,5 +12,5 @@ for (( i=0; i<$rlen; i++));
 do
     echo ${recordsarray[$i]}
     tmux new-window -t $((i+1))
-    tmux send-keys -t $SESSION:$((i+1)) 'python kafka_producer.py '"$IP_ADDR"' '"${recordsarray[$i]}"'' C-m
+    tmux send-keys -t $SESSION:$((i+1)) 'python kafka_producer.py '"${recordsarray[$i]}"'' C-m
 done
