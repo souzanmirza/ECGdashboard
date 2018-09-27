0a0e0e47b85542fbbbdddfde30432f10dc2b2849
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2018-09-27 21:03:02 +0000

    increasing retention time

--- src/kafka/maketopics.sh
+++ src/kafka/maketopics.sh
@@ -1,3 +1,3 @@
 #run this once ssh into kafka cluster master node
-/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic ecg-topic --partitions 4 --replication-factor 2 --config retention.ms=1000
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic ecg-topic --partitions 4 --replication-factor 2 --config retention.ms=600000
 
