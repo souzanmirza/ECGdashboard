a9d5c98f5dfcae864efe93c6d78b922f25088a9c
Souzan <souzanmirza@gmail.com>
2018-09-27 21:57:53 -0400

    edited.

--- src/spark/spawn_spark_consumer.py
+++ src/spark/spawn_spark_consumer.py
@@ -4,6 +4,7 @@ import helpers
 import os
 
 spark_config = helpers.parse_config('../../.config/spark.config')
-ipaddr = spark_config['master-ip-addr'].split(',')
+ipaddr = spark_config['master-ip-addr'].decode().split(',')[0]
 
-os.system("'/usr/local/spark/bin/spark-submit --master spark://%s:7077 --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py'"%(ipaddr))
+
+os.system("/usr/local/spark/bin/spark-submit --master spark://%s --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py"%(ipaddr))
