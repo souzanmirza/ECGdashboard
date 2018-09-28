ba9767fc6fc157aabb4e3c8acc01f7492f3ec37b
Souzan <souzanmirza@gmail.com>
2018-09-27 22:12:35 -0400

    changing run spark bash to run in cluster moder.

--- src/spark/runspark.sh
+++ src/spark/runspark.sh
@@ -1,2 +1,4 @@
-/usr/local/spark/bin/spark-submit --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py
+IP_ADDR="$(hostname)"
+
+/usr/local/spark/bin/spark-submit --master $IP_ADDR --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 ~/ECGdashboard/src/spark/spark_consumer_class.py
 #Pull repo to node first
