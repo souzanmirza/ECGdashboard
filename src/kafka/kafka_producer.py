f0d5a38c4fa16609b1437f8c651676b23ec06493
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2018-09-29 21:41:23 +0000

    returning to old kafka producer

--- src/kafka/kafka_producer.py
+++ src/kafka/kafka_producer.py
@@ -15,54 +15,11 @@ fs = 360
 
 class Producer(object):
 
-    def __init__(self, ip_addr, config_dict):
-        try:
-            self.kafka_config = helpers.parse_config(config_dict['kafka_config'])
-        except Exception as e:
-            self.kafka_config = None
-            print('No kafka config %s' % e)
-        try:
-            self.s3bucket_config = helpers.parse_config(config_dict['s3bucket_config'])
-        except Exception as e:
-            self.s3bucket_config = None
-            print('No s3bucket config %s' % e)
-        try:
-            self.spark_config = helpers.parse_config(config_dict['spark_config'])
-        except Exception as e:
-            self.spark_config = None
-            print('No spark config %s' % e)
+    def __init__(self, ip_addr, kafka_config_infile, s3bucket_config_infile):
+        self.kafka_config = helpers.parse_config(config_dict['kafka_config'])
+        self.s3bucket_config = helpers.parse_config(config_dict['s3bucket_config'])
+        self.producer = KafkaProducer(bootstrap_servers=ip_addr)
 
-        if self.kafka_config is not None:
-            self.topic = self.kafka_config['topic']
-            self.producer = KafkaProducer(bootstrap_servers=ip_addr)
-        elif self.spark_config is not None:
-            self.topic = self.spark_config['topic']
-            self.producer = KafkaProducer(bootstrap_servers=ip_addr,
-                                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        else:
-            print('No topic provided')
-            sys.exit()
-
-    '''
-    def produce_msgs_buffer(self, file_key):
-        msg_cnt = 0
-        while True:
-
-            s3 = boto3.client('s3')
-            obj = s3.get_object(Bucket=self.s3bucket_config['bucket'],
-                                Key="%s_signals.txt" % file_key)['Body']
-
-            messagebuffer = np.chararray((fs + 2), itemsize=10)
-            messagebuffer[0] = file_key
-            messagebuffer[1] = datetime.now().strftime("%Y%m%d-%H%M%S")
-            for i in range(2, fs + 2):
-                # this takes some time to read in...
-                messagebuffer[i] = '%0.3f' % (float(obj._raw_stream.readline().decode().split(' ')[0]))
-            self.producer.send(self.kafka_config['topic'], value=messagebuffer)
-            msg_cnt += 1
-            print(messagebuffer[np.newaxis:, ], len(messagebuffer))
-            time.sleep(0.001)
-    '''
     def produce_ecg_signal_msgs(self, file_key):
         """
         produces messages and sends them to topic
@@ -91,27 +48,18 @@ class Producer(object):
                 except:
                     msg = None
                 if msg is not None:
-                    self.producer.send(self.topic, msg)
+                    self.producer.send(self.kafka_config['topic'], msg)
                     msg_cnt += 1
                 print(message_info)
                 time.sleep(0.001)
 
-    def produce_minibatch_msgs(self, grouped_signal_samples):
-        msg_cnt = 0
-
-        while True:
-            if len(grouped_signal_samples) > 1:
-                self.producer.send(self.topic, grouped_signal_samples)
-                msg_cnt += 1
-            print(grouped_signal_samples)
-            time.sleep(0.001)
-
-
-if __name__ == "__main__":
+   
+if  __name__ == "__main__":
     print('kafka_producer called')
     args = sys.argv
     ip_addr = str(args[1])
     file_key = str(args[2])
-    config_dict = {'kafka_config': '../../.config/kafka.config', 's3bucket_config': '../../.config/s3bucket.config'}
-    prod = Producer(ip_addr, config_dict)
+    kafka_config_infile = '../../.config/kafka.config'
+    s3bucket_config_infile = '../../.config/s3bucket.config'
+    prod = Producer(ip_addr, kafka_config_infile, s3bucket_config_infile)
     prod.produce_ecg_signal_msgs(file_key)
