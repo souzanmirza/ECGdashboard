c0de4676901ebc0fb64114fd310bd4041231189e
Souzan <souzanmirza@gmail.com>
2018-09-30 17:36:17 -0400

    starting two topics.

--- src/kafka/maketopics.sh
+++ src/kafka/maketopics.sh
@@ -1,3 +1,2 @@
-#run this once ssh into kafka cluster master node
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic ecg-topic --partitions 4 --replication-factor 2 --config retention.ms=600000
-
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic hr-topic --partitions 4 --replication-factor 2 --config retention.ms=600000
