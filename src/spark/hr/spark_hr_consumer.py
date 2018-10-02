fde7a3b0ca6e6999c3b76c120d650d47a5f4186d
Souzan <souzanmirza@gmail.com>
2018-10-01 20:58:44 -0400

    Adding in filtering and weight for max peak.

--- src/spark/hr/spark_hr_consumer.py
+++ src/spark/hr/spark_hr_consumer.py
@@ -1,11 +1,6 @@
-import os
 import sys
 
 sys.path.append('../../python/')
-
-# spark_config = helpers.parse_config('../../.config/spark.config')
-
-# os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 consumer.py 52.201.50.203:9092 ecg-data'
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -17,29 +12,43 @@ import psycopg2
 import psycopg2.extras as extras
 import helpers
 import logging
-import json
+import scipy.signal as signal
+
 
 
 def accum(a):
     a.add(1)
     return a.value
 
+def filterECG(ecg):
+    ''' remove baseline'''
+    fftecg=np.fft.fft(ecg)
+    step=500.0/len(fftecg) # which is this 500?
+    s=int(4./step)
+    fftecg[0:s]=0
+    ecg_b=(np.fft.ifft(fftecg)).real
+    """ highpass filter ecg signal with cutoff frequency """
+    b, a = signal.cheby1(2, 1, 2.0/250, 'highpass') # low frequency signals ie gross body movement
+    ecg=signal.lfilter(b, a, ecg_b)
+    b, a = signal.cheby1(2, 1, 60.0/250, 'lowpass') # 60Hz power lines
+    return signal.lfilter(b, a, ecg)
 
-def findHR(ts, ecg):
+
+def findHR(ts, ecg, weight):
     # self.logger.warn('fxn findHR')
     '''detect HR using avg of R-R intervals'''
-    # ecg = filter_ecg(ecg)
-    maxpeak = 0.33 * max(ecg)
+    ecg = filterECG(ecg)
+    maxpeak = weight * max(ecg)
     if maxpeak > 0:
         locs = detect_peaks.detect_peaks(ecg, mph=maxpeak)
         if len(locs) > 1:
-            print('num of peaks is: ', len(locs), ' num of unique peaks are ', len(np.unique(locs)))
-            print(ecg[locs])
+            #print('num of peaks is: ', len(locs), ' num of unique peaks are ', len(np.unique(locs)))
+            #print(ecg[locs])
             Rpeaks_ts = ts[locs]
             Rpeaks_ts.sort()
-            print(Rpeaks_ts[-1], Rpeaks_ts[0], (Rpeaks_ts[-1] - Rpeaks_ts[0]).total_seconds())
+            #print(Rpeaks_ts[-1], Rpeaks_ts[0], (Rpeaks_ts[-1] - Rpeaks_ts[0]).total_seconds())
             diff_ts = np.diff(Rpeaks_ts)
-            print(diff_ts)
+            #print(diff_ts)
             bps = np.sum([diff_ts[i].total_seconds() for i in range(len(diff_ts))]) / len(diff_ts)
             bpm = len(Rpeaks_ts) * (60 / (Rpeaks_ts[-1] - Rpeaks_ts[0]).total_seconds())
             
