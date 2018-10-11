f2e237bcc971eb683eabf26716ee2813042dbc70
Souzan <souzanmirza@gmail.com>
2018-10-11 12:58:19 -0400

    Edited run files to start main_...py

--- src/spark/ecg/runspark.sh
+++ src/spark/ecg/runspark.sh
@@ -2,4 +2,4 @@ IP_ADDR="$(hostname)"
 DEPS="../../python/helpers.py"
 DEPS_ARR=($DEPS)
 
-/usr/local/spark/bin/spark-submit --master spark://$IP_ADDR:7077 --num-executors 3 --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 --py-files $DEPS  spark_ecg_consumer.py
+/usr/local/spark/bin/spark-submit --master spark://$IP_ADDR:7077 --num-executors 3 --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 --py-files $DEPS  main_ecg_consumer.py
